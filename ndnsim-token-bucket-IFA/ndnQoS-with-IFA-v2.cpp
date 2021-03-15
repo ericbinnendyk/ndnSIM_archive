11e1b92fe67f2c5af1b5da1c226657a8dbd3c92d
Eric B <ericabq@gmail.com>
2021-03-14 20:32:44 -0600

    More files

--- ndnsim-token-bucket-IFA/ndnQoS-with-IFA-v2.cpp
+++ ndnsim-token-bucket-IFA/ndnQoS-with-IFA-v2.cpp
@@ -1,3 +1,6 @@
+// Simulates an IFA with settings similar to the testing scenario described in the paper:
+// iCAAP: information-Centric network Architecture for Application-specific Prioritization in Smart Grid
+
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
 #include "ns3/point-to-point-module.h"
@@ -7,6 +10,7 @@ namespace ns3 {
     int
     main(int argc, char* argv[])
     {
+        bool use_token_bucket = /*(argc > 1 && argv[1][0] == 't');*/ false;
        // setting default parameters for PointToPoint links and channels
        Config::SetDefault("ns3::PointToPointNetDevice::DataRate", StringValue("1Mbps"));
        Config::SetDefault("ns3::PointToPointChannel::Delay", StringValue("10ms"));
@@ -41,8 +45,12 @@ namespace ns3 {
        ndn::StrategyChoiceHelper::Install(nodes.Get(0), "/prefix", "/localhost/nfd/strategy/multicast");
        ndn::StrategyChoiceHelper::Install(nodes.Get(1), "/prefix", "/localhost/nfd/strategy/multicast");
        ndn::StrategyChoiceHelper::Install(nodes.Get(2), "/prefix", "/localhost/nfd/strategy/multicast");
-       ndn::StrategyChoiceHelper::Install(nodes.Get(3), "/prefix", "/localhost/nfd/strategy/qos");
-       //ndn::StrategyChoiceHelper::Install(nodes.Get(3), "/prefix", "/localhost/nfd/strategy/multicast");
+       if (use_token_bucket) {
+           ndn::StrategyChoiceHelper::Install(nodes.Get(3), "/prefix", "/localhost/nfd/strategy/qos");
+       }
+       else {
+           ndn::StrategyChoiceHelper::Install(nodes.Get(3), "/prefix", "/localhost/nfd/strategy/multicast");
+       }
        ndn::StrategyChoiceHelper::Install(nodes.Get(4), "/prefix", "/localhost/nfd/strategy/multicast");
        ndn::StrategyChoiceHelper::Install(nodes.Get(5), "/prefix", "/localhost/nfd/strategy/multicast");
 
@@ -53,35 +61,36 @@ namespace ns3 {
        ndn::AppHelper consumerHelper("ns3::ndn::ConsumerCbr");
        // High priority Consumer 
        consumerHelper.SetPrefix("/prefix/typeI");
-       consumerHelper.SetAttribute("Frequency", StringValue("10")); // 10 interests a second
+       consumerHelper.SetAttribute("Frequency", StringValue("90")); // 90 interests a second
        auto apps = consumerHelper.Install(nodes.Get(0));
 
        // Midium priority Consumer 
        consumerHelper.SetPrefix("/prefix/typeII");
-       consumerHelper.SetAttribute("Frequency", StringValue("10")); // 10 interests a second
+       consumerHelper.SetAttribute("Frequency", StringValue("150")); // 150 interests a second
        apps = consumerHelper.Install(nodes.Get(1));
 
        // Low priortiy consumer
        consumerHelper.SetPrefix("/prefix/typeIII");
-       consumerHelper.SetAttribute("Frequency", StringValue("10")); // 10 interests a second
+       consumerHelper.SetAttribute("Frequency", StringValue("300")); // 300 interests a second
        apps = consumerHelper.Install(nodes.Get(2));
 
        // IFA 
        consumerHelper.SetPrefix("/prefix/forged");
-       consumerHelper.SetAttribute("Frequency", StringValue("500")); // 10 interests a second
+       consumerHelper.SetAttribute("Frequency", StringValue("1000")); // 10 interests a second
        apps = consumerHelper.Install(nodes.Get(5));
 
-
-       //Set token bucket values
-       ndn::AppHelper tokenHelper("ns3::ndn::TokenBucketDriver");
-       tokenHelper.SetAttribute("FillRate1", StringValue("75"));
-       tokenHelper.SetAttribute("Capacity1", StringValue("80"));
-       tokenHelper.SetAttribute("FillRate2", StringValue("75"));
-       tokenHelper.SetAttribute("Capacity2", StringValue("80"));
-       tokenHelper.SetAttribute("FillRate3", StringValue("75"));
-       tokenHelper.SetAttribute("Capacity3", StringValue("80"));
-       //consumerHelper.Install(nodes.Get(0));                        // first node
-       apps = tokenHelper.Install(nodes.Get(3));
+       if (use_token_bucket) {
+           //Set token bucket values
+           ndn::AppHelper tokenHelper("ns3::ndn::TokenBucketDriver");
+           tokenHelper.SetAttribute("FillRate1", StringValue("2500"));
+           tokenHelper.SetAttribute("Capacity1", StringValue("1000"));
+           tokenHelper.SetAttribute("FillRate2", StringValue("2000"));
+           tokenHelper.SetAttribute("Capacity2", StringValue("1000"));
+           tokenHelper.SetAttribute("FillRate3", StringValue("800"));
+           tokenHelper.SetAttribute("Capacity3", StringValue("1000"));
+           //consumerHelper.Install(nodes.Get(0));                        // first node
+           apps = tokenHelper.Install(nodes.Get(3));
+      }
 
        // Producer
        ndn::AppHelper producerHelper("ns3::ndn::Producer");
@@ -89,7 +98,7 @@ namespace ns3 {
        producerHelper.SetPrefix("/prefix");
        producerHelper.SetAttribute("PayloadSize", StringValue("1024"));
        producerHelper.Install(nodes.Get(4)); // last node
-   
+
        // App that counts the number of malicious interests reaching the producer
        ndn::AppHelper interestCounterHelper("ns3::ndn::InterestCounter");
        interestCounterHelper.SetPrefix("/prefix/typeIII");
@@ -111,5 +120,6 @@ namespace ns3 {
     int
 main(int argc, char* argv[])
 {
+    std::cout << argc << std::endl;
     return ns3::main(argc, argv);
 }
